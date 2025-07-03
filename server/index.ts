b306087bbf43641738383b79bae48deeecf9d43a
anyangumajimbo <majimboanyangu50@gmail.com>
2025-07-03 10:33:25 +0200

    Use process.env.PORT for Railway deployment

--- server/index.ts
+++ server/index.ts
@@ -59,9 +59,10 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-  server.listen(5000, "0.0.0.0", () => {
-  log(`✅ Server is running at http://localhost:5000`);
+ const port = Number(process.env.PORT) || 5000;
+server.listen(port, () => {
+  log(`Serving on port ${port}`);
 });
+
 
 })();
